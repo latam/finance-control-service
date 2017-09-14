24ee526fdc329e9904b863f41de8a50f24be144e
Mateusz Łata <m_lata@op.pl>
2017-09-14 16:57:28 +0200

    Initial commit with Spring Boot base project.

---
+++ src/main/java/pl/mlata/financecontrolservice/FinanceControlServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.mlata.financecontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceControlServiceApplication.class, args);
+	}
+}
